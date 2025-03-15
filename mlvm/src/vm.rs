6d26d3f6c08d7adfa472889953b604aafb0ea90f
MWaleedK <waleedjaved911@gmail.com>
2025-03-16 01:25:41 +0500

    instructions added and  more tests

--- mlvm/src/vm.rs
+++ mlvm/src/vm.rs
@@ -1,10 +1,44 @@
+use crate::instruction::Opcode;
+
 pub struct VM {
     registers: [i32; 32],
+    pc: usize,
+    program: Vec<u8>,
 }
 
 impl VM {
     pub fn new() -> VM {
-        VM { registers: [0; 32] }
+        VM {
+            registers: [0; 32],
+            program: vec![],
+            pc: 0,
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            // If our program counter has exceeded the length of the program itself, something has
+            // gone awry
+            if self.pc >= self.program.len() {
+                break;
+            }
+            match self.decode_opcode() {
+                Opcode::HLT => {
+                    println!("HLT Encountered");
+                    return;
+                }
+                _ => {
+                    println!("Unrecognized opcode found. Terminating!");
+                    return;
+                }
+            }
+        }
+    }
+
+    fn decode_opcode(&mut self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        return opcode;
     }
 }
 
@@ -19,5 +53,25 @@ mod tests {
     fn test_create_vm() {
         let test_vm = VM::new();
         assert_eq!(test_vm.registers[0], 0);
+    }
+
+    #[test]
+    fn test_opcode_hlt() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![0, 0, 0, 0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        let new_pc_after_one_correct_instruction: usize = 1;
+        assert_eq!(test_vm.pc, new_pc_after_one_correct_instruction);
+    }
+
+    #[test]
+    fn test_opcode_igl() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![200, 0, 0, 0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        let new_pc_after_one_wrong_instruction: usize = 1;
+        assert_eq!(test_vm.pc, new_pc_after_one_wrong_instruction);
     }
 }
