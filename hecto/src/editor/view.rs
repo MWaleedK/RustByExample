d02de7807c2731603c501f99beb5b45c3bdfd3f9
Muhammad Waleed Khan <waleedjaved911@gmail.com>
2025-06-23 21:21:49 -0700

    better formatting

--- hecto/src/editor/view.rs
+++ hecto/src/editor/view.rs
@@ -1,10 +1,10 @@
-use super::terminal::{Position, Size, Terminal};
-use std::io::Error;
+use super::terminal::{Size, Terminal};
+
+mod buffer;
+use buffer::Buffer;
 
 const NAME: &str = env!("CARGO_PKG_NAME");
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-mod buffer;
-use buffer::Buffer;
 
 pub struct View {
     buffer: Buffer,
@@ -17,27 +17,22 @@ impl View {
         self.size = to;
         self.needs_redraw = true;
     }
-    pub fn render_line(at: usize, line_text: &str) -> Result<(), Error> {
-        Terminal::move_caret_to(Position {
-            row: at as u16,
-            col: 0,
-        })?;
-        Terminal::clear_line()?;
-        Terminal::print(line_text)?;
-        Ok(())
+
+    fn render_line(at: usize, line_text: &str) {
+        let result = Terminal::print_row(at, line_text);
+        debug_assert!(result.is_ok(), "Failed to render line");
     }
-    pub fn render(&mut self) -> Result<(), Error> {
+
+    pub fn render(&mut self) {
         if !self.needs_redraw {
-            return Ok(());
+            return;
         }
-
         let Size { height, width } = self.size;
-
         if height == 0 || width == 0 {
-            return Ok(());
+            return;
         }
-
-        let vertical_center = height / 3;
+        #[allow(clippy::integer_division)]
+        let verticlal_center = height / 3;
 
         for current_row in 0..height {
             if let Some(line) = self.buffer.lines.get(current_row) {
@@ -46,24 +41,16 @@ impl View {
                 } else {
                     line
                 };
-                Self::render_line(current_row, truncated_line)?;
-            } else if self.buffer.lines.is_empty() && current_row == vertical_center {
-                Self::render_line(current_row, &Self::build_welcome_message(width))?;
+                Self::render_line(current_row, truncated_line);
+            } else if current_row == verticlal_center && self.buffer.is_empty() {
+                Self::render_line(current_row, &Self::build_welcome_message(width));
             } else {
-                Self::render_line(current_row, "~")?;
+                Self::render_line(current_row, "~");
             }
         }
-
         self.needs_redraw = false;
-        Ok(())
     }
 
-    pub fn load(&mut self, file_name: &str) {
-        if let Ok(buffer) = Buffer::load(file_name) {
-            self.buffer = buffer;
-            self.needs_redraw = true;
-        }
-    }
     fn build_welcome_message(width: usize) -> String {
         if width == 0 {
             return " ".to_string();
@@ -71,33 +58,22 @@ impl View {
         let welcome_message = format!("{NAME} editor --version {VERSION}");
         let len = welcome_message.len();
         if width <= len {
-            return "~".to_string();
+            return " ".to_string();
         }
+        #[allow(clippy::integer_division)]
         let padding = (width.saturating_sub(len).saturating_sub(1)) / 2;
-        let mut full_message = format!("~{}{}", " ".repeat(padding), welcome_message);
+        let mut full_message = format!("{}{}", " ".repeat(padding), welcome_message);
         full_message.truncate(width);
         full_message
     }
-    fn draw_welcome_message() -> Result<(), Error> {
-        let mut welcome_message = format!("{NAME} editor --version {VERSION}");
-        let width = Terminal::size()?.width;
-        let len = welcome_message.len();
-
-        #[allow(clippy::integer_division)]
-        let padding = (width.saturating_sub(len)) / 2;
-
-        let spaces = " ".repeat(padding.saturating_sub(1));
-
-        welcome_message = format!("~{spaces}{welcome_message}");
-        welcome_message.truncate(width);
-        Terminal::print(&welcome_message)?;
-        Ok(())
-    }
-    pub fn draw_empty_row() -> Result<(), Error> {
-        Terminal::print("~")?;
-        Ok(())
+    pub fn load(&mut self, file_name: &str) {
+        if let Ok(buffer) = Buffer::load(file_name) {
+            self.buffer = buffer;
+            self.needs_redraw = true;
+        }
     }
 }
+
 impl Default for View {
     fn default() -> Self {
         Self {
