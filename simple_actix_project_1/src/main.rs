e308e480065529d7f539c3db300a8fdf3f5af03e
Muhammad Waleed Khan <waleedjaved911@gmail.com>
2025-07-04 05:26:19 -0700

    logging eith slog

--- simple_actix_project_1/src/main.rs
+++ simple_actix_project_1/src/main.rs
@@ -1,4 +1,7 @@
 use envconfig::Envconfig;
+use slog::*;
+use slog_async;
+use slog_term;
 
 #[derive(Envconfig)]
 pub struct Config {
@@ -8,10 +11,17 @@ pub struct Config {
     pub api_secret: String,
 }
 
+pub fn setup_logging() -> slog::Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    Logger::root(drain, o!())
+}
+
 fn main() {
     let config = match Config::init_from_env() {
         Ok(v) => v,
         Err(e) => panic!("Could not read config from environment {}", e),
     };
-    println!("Hello, world!");
+    //logging with slog crate
 }
